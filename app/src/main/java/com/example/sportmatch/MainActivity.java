a280dd62dd4e51b6470bf70a0d3a0c20dae03a99
RalucaIonescu12 <ralucaandreeai@yahoo.com>
2024-01-29 18:55:55 +0200

    Completed the unit test

--- app/src/main/java/com/example/sportmatch/MainActivity.java
+++ app/src/main/java/com/example/sportmatch/MainActivity.java
@@ -4,12 +4,15 @@ package com.example.sportmatch;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.Pair;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -33,7 +36,11 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -45,11 +52,19 @@ public class MainActivity extends AppCompatActivity {
     FirebaseDatabase database;
     GoogleSignInClient mGoogleSignInClient;
     int RC_SIGN_IN = 20;
-
     Calendar calendar = Calendar.getInstance();
     int year = calendar.get(Calendar.YEAR);
     int month = calendar.get(Calendar.MONTH) + 1;
     int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+    //for unit testing
+    private static final String KEY_HISTORY_DATA = "KEY_HISTORY_DATA";
+
+    LogHistory mLogHistory;
+    boolean mIsHistoryEmpty = true;
+    private TextView mHistoryTextView;
+    private DateFormat mSimpleDateFormatter;
+    ///
 
 
     //TODO: DUPA SIGN IN SAU SIGN UP DE LEGAT CU FEEDUL
@@ -65,6 +80,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        ///for unit testing
+        mLogHistory = new LogHistory();
+
+        mHistoryTextView = ((TextView) findViewById(R.id.history));
+        mSimpleDateFormatter = new SimpleDateFormat("HH:mm:ss:SSS", Locale.getDefault());
+
+        if (savedInstanceState != null) {
+            // We've got a past state, apply it to the UI.
+            mLogHistory = savedInstanceState.getParcelable(KEY_HISTORY_DATA);
+            for (Pair<String, Long> entry : mLogHistory.getData()) {
+                appendEntryToView(entry.first, entry.second);
+            }
+        }
+        ///
 
 
         //sursa: https://www.youtube.com/watch?v=k_OJt71wEbc&t=260s&ab_channel=DesignWithHassan
@@ -321,4 +353,62 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
     }
+
+
+
+
+
+    //unit testing
+
+
+    /**
+     * Called when the user wants to append an entry to the history.
+     */
+    public void updateHistory(View view) {
+        // Get the text to add and timestamp.
+        EditText editText = (EditText) view.getRootView().findViewById(R.id.editText);
+        CharSequence textToAdd = editText.getText();
+        long timestamp = System.currentTimeMillis();
+
+        // Show it back to the user.
+        appendEntryToView(textToAdd.toString(), timestamp);
+
+        // Update the history.
+        mLogHistory.addEntry(textToAdd.toString(), timestamp);
+
+        // Reset the EditText.
+        editText.setText("");
+    }
+
+
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putParcelable(KEY_HISTORY_DATA, mLogHistory);
+    }
+
+    private void appendEntryToView(String text, long timestamp) {
+        Date date = new Date(timestamp);
+        if (!mIsHistoryEmpty) {
+            mHistoryTextView.append("\n");
+        } else {
+            mHistoryTextView.setText("");
+        }
+        
+        mHistoryTextView.append(String.format("%s [%s]", text, mSimpleDateFormatter.format(date)));
+
+        mIsHistoryEmpty = false;
+    }
 }
+
+
+
+
+
+
